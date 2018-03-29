ff39affcd424d476a8ae1ace01cac6bed9f2ba6a
Sahana Balram <sahana2008balram@gmail.com>
2018-03-29 15:24:32 -0700

    Title Component

---
+++ src/components/Title/Title.js
@@ -0,0 +1,13 @@
+import React,{Component} from "react";
+
+class Title extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Dungeon Crawler</h1>
+            </div>
+        )
+    }
+}
+
+export default Title;
