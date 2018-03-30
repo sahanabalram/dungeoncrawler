e0d86383e494c0ea1be75ab5ffcdda106819a0d7
Sahana Balram <sahana2008balram@gmail.com>
2018-03-29 19:34:53 -0700

    Import Grid Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Title from "./components/Title";
+import Grid from "./components/Grid";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Title/>
+        <Grid/>
       </div>
     );
   }
